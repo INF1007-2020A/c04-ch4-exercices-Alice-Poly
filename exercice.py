11f66dc0a4cdcf2cfcaa87545368c84af04c9fb1
Alice-Poly <jing-min.huang@polymtl.ca>
2020-09-15 22:58:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,9 +6,9 @@ def is_even_len(string: str) -> bool:
     count = len(string)
     x = count/2
     if type(x) = int
-        print(Even Length)
+        print(Even Length")
     else:
-        print
+        print("Not even length")
     pass
 
 
